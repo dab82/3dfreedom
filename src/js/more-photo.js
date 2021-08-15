fbfe3ef8df6501f0adaf7c907221518743dc89d7
ITigor1985 <xromer85@gmail.com>
2021-08-15 15:41:33 +0300

    js

---
+++ src/js/more-photo.js
@@ -0,0 +1,6 @@
+$('.masonry').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{enabled:true}
+    // other options
+  });
